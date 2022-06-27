2f4a5a5e263c57c452b32f396cc51891206f9432
Robins-2603 <robinsforwork@gmail.com>
2022-06-28 01:23:52 +0530

    Added

---
+++ src/com/AddressBook2/AddressBookMain2.java
@@ -0,0 +1,7 @@
+package com.AddressBook2;
+
+public class AddressBookMain2 {
+    public static void main(String[] args) {
+        System.out.println("-------------Welcome to Address Book---------------" );
+    }
+}
---
+++ src/com/AddressBook2/Contact.java
@@ -0,0 +1,12 @@
+package com.AddressBook2;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String Address;
+    private String City;
+    private String state;
+    private String zip;
+    private String PhoneNo;
+    private String emailId;
+}
